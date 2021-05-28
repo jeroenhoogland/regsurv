e4506b487247735226ff011352062167a1724d41
JHoogland <jhoogland.git@outlook.com>
2021-05-28 19:52:39 +0200

    Additional tests for rcs; now only works for at least 3 knot locations (2 outer and 1 inner).
    NB: might include option knots=NULL to just return a single column matrix X[ ,1] <- x

--- R/rcs.R
+++ R/rcs.R
@@ -4,8 +4,9 @@
 #' columns based on a truncated power series (as described in section 2.4.5 of the second edition of
 #' Regression Modeling Strategies by Frank E. Harrrell, Jr).
 #'
-#' @param x numeric
-#' @param knots numeric
+#' @param x numeric vector
+#' @param knots numeric vector including at least 3 unique knot locations of increasing order,
+#' with the outer ones indicating the boundary knots.
 #'
 #' @return matrix of basis columns
 #' @export
@@ -22,6 +23,18 @@ rcs <- function(x, knots = stats::quantile(x, probs=c(0,0.1,0.5,0.9,1))){
     stop("x should have length >= 2")
   }
 
+  if(!is.numeric(knots)){
+    stop("k should be a numeric vector")
+  }
+
+  if(length(unique(knots)) < 3){
+    stop("provide a minium of 3 unique knots")
+  }
+
+  if(!isTRUE(all.equal(order(knots), 1:length(knots)))){
+    stop("provide knots in increasing order")
+  }
+
   k <- length(knots)
 
   if(length(unique(knots)) != k){
@@ -32,6 +45,7 @@ rcs <- function(x, knots = stats::quantile(x, probs=c(0,0.1,0.5,0.9,1))){
 
   X <- matrix(NA, nrow = length(x), ncol = k-1)
   X[ , 1] <- x
+
   for(j in 1:(k-2)){
     scale1 <- (knots[k] - knots[j]) / (knots[k] - knots[k-1])
     scale2 <- (knots[k-1] - knots[j]) / (knots[k] - knots[k-1])
@@ -41,5 +55,6 @@ rcs <- function(x, knots = stats::quantile(x, probs=c(0,0.1,0.5,0.9,1))){
   }
   tau <- (knots[k] - knots[1])^2
   X[ ,2:(k-1)] <- X[ ,2:(k-1)] / tau # scale such that all cols are on scale of X
+
   return(X)
 }
--- tests/testthat/test-rcs.R
+++ tests/testthat/test-rcs.R
@@ -3,3 +3,11 @@ test_that("rcs returns error when input is not a numeric vector of length > 1", 
   expect_error(rcs(1))
 })
 
+test_that("knots is numeric, has length >= 3, and is in the correct order ", {
+  expect_error(rcs(1:10, 1))
+  expect_error(rcs(1:10, 3:1))
+})
+
+test_that("warning on duplicate knots", {
+  expect_warning(rcs(1:10, c(1,1,2,3)))
+})
